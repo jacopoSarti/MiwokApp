4d6e0405a817a6a30fc57468d3fc7c333213487a
Jacopo Sarti <jacopo.sarti90@gmail.com>
2018-11-14 13:45:40 +0800

    Added Media Player to all classes, Modified Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,17 +7,20 @@ public class Word {
     private String mEnglishTranslation;
     private String mMiwokTranslation;
     private int mImageId;
+    private int mAudioId;
 
-    public Word(String english, String miwok) {
+    public Word(String english, String miwok, int audioId) {
         mEnglishTranslation = english;
         mMiwokTranslation = miwok;
         mImageId = 0;
+        mAudioId = audioId;
     }
 
-    public Word(String english, String miwok, int imageId) {
+    public Word(String english, String miwok, int imageId, int audioId) {
         mEnglishTranslation = english;
         mMiwokTranslation = miwok;
         mImageId = imageId;
+        mAudioId = audioId;
     }
 
     public String getmEnglishTranslation() {
@@ -29,4 +32,5 @@ public class Word {
     }
 
     public int getmImageId() { return this.mImageId; }
+    public int getmAudioId() { return this.mAudioId; }
 }
